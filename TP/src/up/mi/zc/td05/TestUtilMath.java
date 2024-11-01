2ebbe538890c5522fe0d88848a743d9e5a314d09
Zak1452 <zakariachaker@yahoo.fr>
2024-11-01 23:42:02 +0100

    Commit TD5 EX2

---
+++ TP/src/up/mi/zc/td05/TestUtilMath.java
@@ -0,0 +1,97 @@
+package up.mi.zc.td05;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TestUtilMath {
+	public static void affichageMenu()
+	{
+		System.out.println("Programmme UtilMath\n");
+		System.out.println("\n1. Somme de 3 entiers.");
+		System.out.println("\n2. Calcul factorielle d'un entier.");
+		System.out.println("\n3. Calcul combinaison de deux entiers p et n.");
+		System.out.println("\n4. Calcul a la puissance m d'un entier n");
+		System.out.println("\n5. Sortie du programme");
+		System.out.println("\n\nSaisir votre choix: ");
+	}
+	
+	public static void main(String [] args)
+	{
+		Scanner saisie = new Scanner(System.in);
+		int choix = 0;
+		boolean choixBoot = true;
+		while(choix != 5)
+		{
+			try {
+				if(choixBoot)
+				{
+					affichageMenu();
+					choix = saisie.nextInt();
+				}
+			switch(choix)
+			{
+			case 1:
+			{
+				int a = 0, b = 0, c = 0;
+				choixBoot = false;
+				System.out.println("Saisir 3 entiers: \na: ");
+				a = saisie.nextInt();
+				System.out.println("\nb: ");
+				b = saisie.nextInt();
+				System.out.println("\nc: ");
+				c = saisie.nextInt();
+				System.out.println("La somme des 3 entiers vaut: "+ UtilMath.somme3(a, b, c)); 
+				choixBoot = true;
+				break;
+			}
+			case 2:
+			{
+				choixBoot = false;
+				System.out.println("Saisir n: ");
+				int n = saisie.nextInt();
+				System.out.println("La factorielle de n vaut: "+UtilMath.factRec(n));
+				choixBoot = true;
+				break;
+			}
+			case 3:
+			{
+				choixBoot = false;
+				System.out.println("Saisir un entier n et p (avec n > p)\nn: ");
+				int n = saisie.nextInt();
+				System.out.println("\np: ");
+				int p = saisie.nextInt();
+				System.out.println("La combinaison de n et p vaut: "+UtilMath.comb(n, p));
+				choixBoot = true;
+				break;
+			}
+			case 4:
+			{
+				choixBoot = false;
+				System.out.println("Saisir un entier n et m (avec m la puissance de n)\nn: ");
+				int n = saisie.nextInt();
+				System.out.println("\nm: ");
+				int m = saisie.nextInt();
+				System.out.println(n +" a la puissance "+m+" vaut: "+UtilMath.puissance(n, m));
+				choixBoot = true;
+				break;
+			}
+			case 5:
+			{
+				System.out.println("\n\nSortie du programme");
+				break;
+			}
+			default:
+			{
+				System.out.println("\nErreur, valeur saisie incorrecte");
+				break;
+			}
+			}
+			}catch(InputMismatchException e)
+			{
+				System.err.println("Erreur type saisi incorrect");
+				saisie.next();
+			}
+		}
+		saisie.close();
+	}
+}
--- TP/src/up/mi/zc/td05/UtilMath.java
+++ TP/src/up/mi/zc/td05/UtilMath.java
@@ -1,5 +1,149 @@
 package up.mi.zc.td05;
 
 public class UtilMath {
-
+		/**
+		 * 
+		 * @param a
+		 * @param b
+		 * @param c
+		 * @return somme de 3 entiers
+		 * @description Version améliorée avec Scanner pour saisie
+		 */
+		public static int somme3(int a, int b, int c)
+		{
+			return (a+b+c);
+		}
+		/**
+		 * 
+		 * @param n
+		 * @return factorielle de n
+		 * @Exemple
+		 * n = 7
+		 * 1*2*3*4*5*6
+		 */
+		public static long factIt(int n)
+		{
+			int res = 1;
+			if (n == 0 || n == 1)
+				return res;
+			else 
+			{
+				for (int i = 0; i<n;i++)
+				{
+					res = res * (n-i);
+				}
+				return res;
+			}
+		}
+		/**
+		 * 
+		 * @param n
+		 * @description Méthode récursive 
+		 * @return la factorielle de n entré en parametre
+		 * 
+		 */
+		public static long factRec(int n) throws IllegalArgumentException
+		{
+			if(n<0)
+				throw new IllegalArgumentException("Erreur, impossible de calculer la factorielle d'un entier négatif");
+			if (n== 0 || n== 1)
+				return 1;
+			return n*factRec(n-1);
+		}
+		/**
+		 * 
+		 * @param n
+		 * @param p
+		 * @return Combinaison de deux entiers positifs p et n
+		 */
+		public static long comb (int n, int p) throws IllegalArgumentException
+		{
+			if(p > n)
+				throw new IllegalArgumentException("Erreur, p ne peut pas etre plus grand que n pour calcul de la combinaison.");
+			long res = 0;
+			if (p<=n)
+			{
+				try
+				{
+					res = (factRec(n))/(factRec(p)*factRec(n-p));
+				}catch(Exception e)
+				{
+					System.err.println(e.getMessage()+" Pour plus de détails: \n");
+					e.printStackTrace();
+				}
+				
+				return res;
+			}
+			else 
+				return res;
+		}
+		
+		public static long puissance(int n, int m) throws IllegalArgumentException
+		{
+			if(m < 0)
+				throw new IllegalArgumentException("Erreur, m ne peut pas etre negatif (puissance)");
+			if (m>=0)
+			{
+				if (m==0)
+					return 1;
+				for (int i=0; i<m-1; i++)
+				{
+					n *= n;
+				}
+				return n;
+			}
+			else 
+				return 0;
+		}
+		//Exercice 3 MAXIMUM
+		/**
+		 * 
+		 * @param a
+		 * @param b
+		 * @return
+		 */
+		public static int max2(int a, int b)
+		{
+			//Condition ternaire
+			return ((a>b) ? a : b);
+		}
+		/**
+		 * 
+		 * @param a
+		 * @param b
+		 * @param c
+		 * @return max de 3 entiers.
+		 */
+		public static int max3v1(int a, int b, int c)
+		{
+			int max;
+			if (a > b)
+			{
+				if (a > c)
+					max = a;
+				else 
+					max = c;
+				return max;
+			}
+			else
+			{
+				if(b > c)
+					max = b;
+				else 
+					max = c;
+				return max;
+			}
+		}
+		public static int max3v2(int a, int b, int c)
+		{
+			/*
+			 * Méthode qui utilise le max2
+			 */
+			if (max2(a, b)> c)
+				return max2(a,b);
+			else
+			{
+				return c;
+			}
+		}
 }
