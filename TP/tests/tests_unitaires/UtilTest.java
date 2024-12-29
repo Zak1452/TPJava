f219cdd08773958b94b5ab560bf88334c218a28e
Zak1452 <zakariachaker@yahoo.fr>
2024-12-29 09:10:09 +0100

    Commit Cours tests Unitaires

--- TP/tests/tests_unitaires/UtilTest.java
+++ TP/tests/tests_unitaires/UtilTest.java
@@ -1,12 +1,16 @@
 package tests_unitaires;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import up.mi.zc.ClasseUtilitaire1;
 import up.mi.zc.cm06.Util;
 
 public class UtilTest {
@@ -40,5 +44,19 @@ public class UtilTest {
     public void testExceptionNonLevee()
     {
     	assertDoesNotThrow(()->Util.getStringFromCharException(2, 'd'));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"bob", "hh", "helloolleh"})
+    public void test(String word)
+    {
+    	assertTrue(ClasseUtilitaire1.isPalindrome(word));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"descartes", "toto", "bonjour"})
+    public void testNotPalindrome(String word)
+    {
+    	assertFalse(ClasseUtilitaire1.isPalindrome(word));
     }
 }
